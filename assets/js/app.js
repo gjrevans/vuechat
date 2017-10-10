8318c183d1d8fc03c5e911e2bc800c4ca8070c46
Gordon Evans <gjrevans@gmail.com>
2017-10-09 21:33:57 -0400

    Add users to socket.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,14 @@
 import "phoenix_html"
-import socket from "./socket"
+import Vue from 'vue'
+import MyApp from "./components/my-app.vue"
+
+// Create the main component
+Vue.component('my-app', MyApp)
+
+// And create the top-level view model:
+new Vue({
+  el: '#app',
+  render(createElement) {
+    return createElement(MyApp, {})
+  }
+});
--- assets/js/components/my-app.vue
+++ assets/js/components/my-app.vue
@@ -1,32 +1,58 @@
 <template>
   <div class="my-app">
     <h1>Vuechat</h1>
-    <ul v-for="message in messages">
-      <li>
-        <small>{{message.received_at}}</small>: {{message.body}}
-      </li>
-    </ul>
-    <input type="text" v-model="message" v-on:keyup.13="sendMessage">
+    <div class="user-details" v-if="enterName">
+      <label>Please enter your name:</label><br>
+      <input type="text" v-model="username">
+      <button v-on:click="connectToChat">Next</button>
+    </div>
+    <div class="messages" v-else>
+      <ul v-for="message in messages">
+        <li>
+          <small>{{message.received_at}}</small>: <strong>{{message.username}}</strong>: {{message.body}}
+        </li>
+      </ul>
+      <strong>{{username}}:</strong><br/>
+      <input type="text" v-model="message" v-on:keyup.13="sendMessage">
+    </div>
   </div>
 </template>
 
 <script>
+import {Socket} from "phoenix"
+
 export default {
   data() {
     return {
-      message: ""
-    }
-  },
-  computed: {
-    messages() {
-      return this.$parent.messages
+      socket: null,
+      channel: null,
+      messages: [],
+      message: "",
+      username: "",
+      enterName: true
     }
   },
   methods: {
     sendMessage() {
-      this.$parent.channel.push("new_msg", { body: this.message })
+      this.channel.push("new_msg", { body: this.message })
       this.message = ''
     },
+    connectToChat() {
+      this.enterName = false
+      this.socket = new Socket("/socket", {params: {username: this.username}})
+
+      this.socket.connect()
+
+      this.channel = this.socket.channel("room:lobby", {})
+      this.channel.on("new_msg", payload => {
+        payload.received_at = Date();
+        this.messages.push(payload);
+      });
+
+      this.channel.join()
+        .receive("ok", response => { console.log("Joined successfully", response) })
+        .receive("error", response => { console.log("Unable to join", response) })
+    }
   }
 }
 </script>
@@ -35,7 +61,7 @@ export default {
 .my-app {
   margin-left: auto;
   margin-right: auto;
-width: 800px;
+  width: 800px;
   h1 {
     text-align: center;
   }
